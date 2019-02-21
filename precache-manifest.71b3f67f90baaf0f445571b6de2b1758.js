0b69cab89f200b2c566550f23cd8460fba3f1e3f
Balazs Kosa <b4l4g3@gmail.com>
2019-02-21 13:45:41 +0100

    Updates

--- precache-manifest.71b3f67f90baaf0f445571b6de2b1758.js
+++ precache-manifest.71b3f67f90baaf0f445571b6de2b1758.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/taco-bar/static/js/runtime~main.cf76bf07.js"
   },
   {
-    "revision": "3407c87577a282f60340",
-    "url": "/taco-bar/static/js/main.3407c875.chunk.js"
+    "revision": "0ced0862df4a0fb0d70a",
+    "url": "/taco-bar/static/js/main.0ced0862.chunk.js"
   },
   {
     "revision": "5ed6050ceff73acf7c8e",
     "url": "/taco-bar/static/js/1.5ed6050c.chunk.js"
   },
   {
-    "revision": "a4d26e49ea952bdc5fd2eab54be85818",
+    "revision": "706cd392a7ea2f8b8770a617a1ce7658",
     "url": "/taco-bar/index.html"
   }
 ];
